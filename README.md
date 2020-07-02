3646fd15b06db9a081f2e8f4a4ff8747541959f8
Nazmi Aydogdu <nazmi@grapheek.com>
2020-07-02 16:14:14 +0200

    update 02-07, colorbox component + styling

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# Easily change all country flags !
+# Change the World !
 
-Work in progress...
+Easily change the colors of all flags of the world.
+
+Works in progress :
+* Extract colors when building SVG components
+* Use localstorage to keep user color changes
+* Add reset for user changes
+* Improve UI
 
 ## Features
+
+Easily change colors from Flags. 
 
 ## Credits
 Flags get via https://flagicons.lipis.dev/
--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react"
+import Colorbox from "./Colorbox"
 
 class Flag extends Component {
+  constructor() {
+    super();
+    this.state = {
+      colors : {
+        1: '',
+        2: '',
+        3: ''
+      },
+      focused: false
+    }
+  }
+
+  handleChangeColor = e => {
+    let newState = Object.assign({}, this.state)
+    const i = e.target.getAttribute('data-key')
+    newState.colors[i] = e.target.value
+    this.setState(newState)
+  }
+
+  handleChangeStyleOnFocus = () => this.setState({
+    focused: true
+  })
+
+  handleChangeStyleOnBlur = () => this.setState({
+    focused: false
+  })
+
+  generateColorboxes = () => {
+    let boxes = []
+    for (let i = 1; i <= Object.keys(this.state.colors).length; i++)
+      boxes.push(
+        <div key={i}>
+          <Colorbox
+            dataKey={i}
+            value={this.state.colors.i}
+            onChangeColor={this.handleChangeColor}
+            onFocus={this.handleChangeStyleOnFocus}
+            onBlur={this.handleChangeStyleOnBlur}
+            holder={"Color "+ i }
+          />
+        </div>
+      )
+    return boxes
+  }
+
   render() {
     const FlagName = this.props.fn
     return (
-      <div className="flag w-40 p-5 my-2 bg-white rounded-lg shadow">
-        <div className="shadow rounded-lg overflow-hidden">
+      <div className={'flag-wrapper w-40 p-5 my-2 bg-white rounded-lg shadow'+(this.state.focused ? ' focused' : '')}>
+        <div data-color={this.state.color} className="flag-item shadow rounded-lg overflow-hidden transition-all duration-200 delay-100">
           <FlagName />
         </div>
+        {this.generateColorboxes()}
       </div>
     )
   }
